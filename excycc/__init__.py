50cbd96a0cc8f95e5864aa1e87543559c4a5579f
excaeruleo <prototyped625@gmail.com>
2026-07-13 22:40:14 -0400

    Added termForwardCurves.py to excycc/__init__.py

--- excycc/__init__.py
+++ excycc/__init__.py
@@ -6,6 +6,7 @@ from contCompYields import calcYields
 from contCompYields import graph as graphCC
 from forwardCurve import forwardCurve
 from forwardCurve import graph as graphFC
+from termForwardCurves import graph as graphTFC
 
 def main():
   periodDF = calcDisFact('../csvs/daily-treasury-par-yield-curve-rates.csv')
@@ -17,6 +18,8 @@ def main():
   forwardCurveDF = forwardCurve('../csvs/daily-treasury-par-yield-curve-rates.csv')
   print(forwardCurveDF)
   graphFC(forwardCurveDF)
+  termForwardCurveDF = forwardCurve('../csvs/daily-treasury-par-yield-curve-rates.csv')
+  graphTFC(termForwardCurveDF)
   pass
 
 if __name__ == "__main__":
