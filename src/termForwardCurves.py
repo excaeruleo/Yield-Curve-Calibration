d3b04cab38bd0fe8d50a6fb83b1fd766db0f4b71
excaeruleo <prototyped625@gmail.com>
2026-07-28 21:25:54 -0400

    Fixed typo in print statement

--- src/termForwardCurves.py
+++ src/termForwardCurves.py
@@ -21,7 +21,7 @@ def graph(df):
   tenors = [1, 5, 10, 20]
   denseTimes = np.linspace(maturitiesNumeric[0], maturitiesNumeric[-1], 500)
   _, ((ax1, ax5), (ax10, ax20)) = plt.subplots(2, 2)
-  
+
   for date in df.index:
     discount = df.loc[date].to_numpy(dtype = float)
     spline = CubicSpline(maturitiesNumeric, np.log(discount))
@@ -30,14 +30,14 @@ def graph(df):
       t = denseTimes[denseTimes + tau <= maturitiesNumeric[-1]]
       forward = (spline(t) - spline(t + tau)) / tau
       ax.set_xlabel('t')
-      ax.set_ylabel(f"f(0, t, t + {tau}")
+      ax.set_ylabel(f"f(0, t, t + {tau})")
       ax.set_title(f"{tau}y continuously compounded Treasury forward rates")
       ax.grid(True)
       ax.plot(t, forward, label = date)
       ax.legend()
-  
+
   plt.show()
-   
+
 
 def main():
   forwardCurves = forwardCurve('../csvs/daily-treasury-par-yield-curve-rates.csv')
